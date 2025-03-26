e847d2f33e1186265ea8a0286de0ec697f313bb8
abhijit1458 <23f3001458@ds.study.iitm.ac.in>
2025-03-26 20:24:36 +0000

    name

--- app.py
+++ app.py
@@ -13,9 +13,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
 @app.get("/api")
 async def read_api():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World from api!"}
+
+@app.get("/name")
+async def read_name(name: str):
+    return {"message": f"Hello, {name}!"}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
